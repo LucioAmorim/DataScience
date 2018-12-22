604d26428f315bdf2f173e8289429ad7c4c312c6
LucioAmorim <lucioaa@outlook.com>
2018-12-21 23:53:34 -0200

    Add files via upload
    Curso Alura - Classificação

---
+++ carregar_acessos.py
@@ -0,0 +1,15 @@
+import csv
+def carregar_acessos():
+  X = []
+  Y = []
+
+  arquivo = open('acesso.csv', 'r')
+  leitor = csv.reader(arquivo)
+
+  next(leitor)
+
+  for acessou_home,acessou_como_funciona,acessou_contato, comprou in leitor:
+    X.append([int(acessou_home),int(acessou_como_funciona),int(acessou_contato)])
+    Y.append(int(comprou))
+
+  return X, Y
